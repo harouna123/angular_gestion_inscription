108981e70b973a03f8b35a79323e1e4fcc608fa6
thierno <thiernoharouna2300@gmail.com>
2023-01-11 21:14:41 +0000

    premier enregistrement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DepartementModule } from './departement/departement.module';
+import { FiliereModule } from './filiere/filiere.module';
+import { ClasseModule } from './classe/classe.module';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FiliereDepartementComponent } from './filiere-departement/filiere-departement.component';
+import { ClasseFiliereComponent } from './classe-filiere/classe-filiere.component';
+import { DossierModule } from './dossier/dossier.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AcceuilComponent,
+    NavbarComponent,
+    FiliereDepartementComponent,
+    ClasseFiliereComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DepartementModule,
+    FiliereModule,
+    ClasseModule,
+    HttpClientModule,
+    DossierModule
   ],
   providers: [],
   bootstrap: [AppComponent]
